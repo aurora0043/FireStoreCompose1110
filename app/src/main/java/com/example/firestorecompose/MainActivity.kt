2376b328b5594e358521ce8cc0ccb3a671dec3f4
Aurora0043 <940403army@gmail.com>
2024-11-10 19:16:24 +0800

    v6

--- app/src/main/java/com/example/firestorecompose/MainActivity.kt
+++ app/src/main/java/com/example/firestorecompose/MainActivity.kt
@@ -13,6 +13,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firestorecompose.ui.theme.FireStoreComposeTheme
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.material3.Button
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.text.input.KeyboardType
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +36,114 @@ class MainActivity : ComponentActivity() {
         setContent {
             FireStoreComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Birth(m = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+
 @Composable
-fun GreetingPreview() {
-    FireStoreComposeTheme {
-        Greeting("Android")
+fun Birth(m: Modifier){
+    var userName by remember { mutableStateOf("林政彥")}
+    var userWeight by remember { mutableStateOf(4000)}
+    var userPassword by remember { mutableStateOf("")}
+    var msg by remember { mutableStateOf("訊息")}
+    val db = Firebase.firestore
+
+    Column {
+        TextField(
+            value = userName,
+            onValueChange = { newText ->
+                userName = newText
+            },
+            modifier = m,
+            label = { Text("姓名") },
+            placeholder = { Text("請輸入您的姓名") }
+
+        )
+        TextField(
+            value = userWeight.toString(),
+            onValueChange = { newText ->
+                if (newText == ""){
+                    userWeight = 0
+                }
+                else{userWeight = newText.toInt()}
+            },
+            label = { Text("出生體重") },
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Number)
+        )
+        TextField(
+            value = userPassword,
+            onValueChange = { newText ->
+                userPassword = newText
+            },
+            label = { Text("密碼") },
+            placeholder = { Text(text = "請輸入您的密碼") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Password)
+        )
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"
+                + "\n密碼：$userPassword")
+        Row {
+            Button(onClick = {
+                val user = Person(userName, userWeight, userPassword)
+                db.collection("users")
+                    //.add(user)
+                    .document(userName)
+                    .set(user)
+                    .addOnSuccessListener { documentReference ->
+                        msg = "新增/異動資料成功"
+                    }
+                    .addOnFailureListener { e ->
+                        msg = "新增/異動資料失敗：" + e.toString()
+                    }
+            }) {
+                Text("新增/修改資料")
+            }
+            Button(onClick = {
+                db.collection("users")
+                    //.whereEqualTo("userName", userName)
+                    //.whereLessThan("userWeight", userWeight)
+                    .orderBy("userWeight", Query.Direction.DESCENDING)
+                    .limit(2)
+                    .get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            msg = ""
+                            for (document in task.result!!) {
+                                msg += "文件id：" + document.id + "\n名字：" + document.data["userName"] +
+                                        "\n出生體重：" + document.data["userWeight"].toString() + "\n\n"
+                            }
+                            if (msg == "") {
+                                msg = "查無資料"
+                            }
+                        }
+                    }
+            }) {
+                Text("查詢資料")
+            }
+            Button(onClick = {
+                db.collection("users")
+                    .document(userName)
+                    .delete()
+                msg = "刪除資料"
+            }) {
+                Text("刪除資料")
+            }
+        }
+        Text(text = msg)
     }
 }
+
+
+data class Person(
+    var userName: String,
+    var userWeight: Int,
+    var userPassword: String
+)
